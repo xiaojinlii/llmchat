27a9bc40e279d4258dfb8927771c6fca22f2899b
JinLiGame <553555614@qq.com>
2024-02-27 17:20:54 +0800

    报错修复

--- llmchat/test/chat_test.py
+++ llmchat/test/chat_test.py
@@ -3,6 +3,7 @@ from langchain_openai import ChatOpenAI
 model = ChatOpenAI(
     model_name="cyou-api",
     openai_api_base="http://127.0.0.1:20000/v1",
+    openai_api_key="empty"
 )
 
 res = model.invoke("你是谁")
